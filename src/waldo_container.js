ef78a637aaea3d0086cdafabaf823c1f01ea53cf
JustWaveThings <justwavethings@proton.me>
2023-06-26 10:24:55 -0400

    Canvas image added

--- src/waldo_container.js
+++ src/waldo_container.js
@@ -1,4 +1,5 @@
 // this will hold the container that renders the image and an invisible grid of divs with event listeners
+import waldoImg from './img/findjohnoliver.jpg';
 
 function waldoContainer() {
   // set height and width for 'waldo container' which the image and div will inherit
@@ -11,6 +12,11 @@ function waldoContainer() {
   const imageCont = document.createElement('div');
   imageCont.className = 'imageCont';
   body.appendChild(imageCont);
+
+  const canvasImage = document.createElement('img');
+  canvasImage.src = waldoImg;
+  canvasImage.className = 'waldoImg';
+  imageCont.appendChild(canvasImage);
 
   for (let i = 0; i <= containerSize / divisor; i += 1) {
     const row = document.createElement('div');
@@ -29,5 +35,4 @@ function waldoContainer() {
     }
   }
 }
-
 export default waldoContainer;
