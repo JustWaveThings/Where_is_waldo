55cc37bbe0d2f2b3245d4cc65b959522eb43e166
JustWaveThings <justwavethings@proton.me>
2023-06-29 09:48:07 -0400

    Remove static adjustment for x axis coords due to issues with zoom

--- src/update_window_location.js
+++ src/update_window_location.js
@@ -3,6 +3,6 @@ export default function updateWindowLocation(coords) {
   const listContainer = document.querySelector('.listContainer');
 
   coords && (listContainer.style.top = `${coords[1]}px`);
-  coords && (listContainer.style.left = `${coords[0] - 360}px`);
+  coords && (listContainer.style.left = `${coords[0]}px`);
   coords && (listContainer.style.zIndex = '3');
 }
